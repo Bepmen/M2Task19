31d2c74b8a51a63d4839faca113382c9516f5979
Александр Мигилев <e-yh@outlook.com>
2022-02-05 15:17:22 +0300

    Добавьте файлы проекта.

---
+++ M2Task19.1/ViewModels/MainWinowYiewModel.cs
@@ -0,0 +1,62 @@
+﻿using M2Task19._1.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace M2Task19._1.ViewModels
+{
+    internal class MainWinowYiewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double radius;
+        private double circumference;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Circumference
+        {
+            get => circumference;
+            set
+            {
+                circumference = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+
+        private void OnAddCommandExecute(object p)
+        {
+            Circumference = Ariph.Add(Radius);
+        }
+        
+        private bool CanAddCommandExecuted(object p)
+        {
+            return
+                true;
+        }
+        
+        public MainWinowYiewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+    }
+}
